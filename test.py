b9a704f5cdb6cb544347819e7c943b9772daba51
chintubhoi2 <bhoi.chintu@gmail.com>
2023-05-25 21:28:03 +0530

    final commit

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ model = pickle.load(open(r'C:\Users\bhoic\OneDrive\Desktop\quora\model.pkl','rb'
 scaler = pickle.load(open(r'C:\Users\bhoic\OneDrive\Desktop\quora\scaler.pkl','rb'))
 
 q1 = "Astrology: I am a Capricorn Sun Cap moon and cap rising...what does that say about me?'"
-q2 = "Astrology: I am a Capricorn Sun Cap moon and cap rising...what does that say about me?'"
+q2 = "Astrology: I am a Capricorn"
 
 query = helper.query_point_creator(q1,q2)
 print(query)
